cb4ea0d2bd5b1a2cb536d17249c672384aca4145
joaolavelino <joaolavelino@gmail.com>
2022-01-16 18:48:32 -0300

    6. Fetching other games

--- src/api.js
+++ src/api.js
@@ -30,5 +30,9 @@ console.log(currentDate, lastYear, nextYear);
 
 //popular games
 const popular_games = `games?key=${process.env.REACT_APP_RAWG_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${process.env.REACT_APP_RAWG_API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const new_games = `games?key=${process.env.REACT_APP_RAWG_API_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesUrl = () => `${base_url}${popular_games}`;
+export const upcomingGamesUrl = () => `${base_url}${upcoming_games}`;
+export const newGamesUrl = () => `${base_url}${new_games}`;
--- src/redux/actions/gamesAction.js
+++ src/redux/actions/gamesAction.js
@@ -1,15 +1,19 @@
 import axios from "axios";
-import { popularGamesUrl } from "../../api";
+import { popularGamesUrl, upcomingGamesUrl, newGamesUrl } from "../../api";
 
 //While working with THUNK to make async request with Axios, you need to call another arrow function inside the initial arrow function of the action creator.
 //This second arrow function will be asynchronous and will contain the axios request.
 export const loadGames = () => async (dispatch) => {
   //Fetch
   const popularData = await axios.get(popularGamesUrl());
+  const upcomingData = await axios.get(upcomingGamesUrl());
+  const newData = await axios.get(newGamesUrl());
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newData.data.results,
     },
   });
 };
