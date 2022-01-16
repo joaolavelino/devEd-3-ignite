d665a31d50ddc66708652086173cd37356a8e455
joaolavelino <joaolavelino@gmail.com>
2022-01-16 15:23:55 -0300

    2. Setting Up Our Api

---
+++ src/api.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+//BASE URL
+const base_url = "https://api.rawg.io/api/";
+
+//Getting the current time
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+console.log(currentDate, lastYear, nextYear);
+
+//popular games
+const popular_games = `games?key=${process.env.REACT_APP_RAWG_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
